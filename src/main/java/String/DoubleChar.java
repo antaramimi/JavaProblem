1867dc4ede85e8331013cb2b51fc0a1fc52b5c38
Antara <antaramimi@gmail.com>
2020-04-15 21:58:44 +0100

    Added more java problems

---
+++ src/main/java/String/DoubleChar.java
@@ -0,0 +1,21 @@
+package String;
+
+/**
+ * Question:
+ * Given a string, return a string where for every char in the original, there are two chars.
+ * Examples:
+ * doubleChar("The") → "TThhee"
+ * doubleChar("AAbb") → "AAAAbbbb"
+ * doubleChar("Hi-There") → "HHii--TThheerree"
+ */
+public class DoubleChar {
+
+    public String doubleChar(String str) {
+        String newString = "";
+        for (int i = 0; i < str.length(); i++) {
+            newString = newString + str.charAt(i) + str.charAt(i);
+        }
+        return newString;
+    }
+
+}
