f54ab378b073ed0cb4b5659ccff868d296d89143
Antara <antaramimi@gmail.com>
2020-04-17 18:40:33 +0100

    Adding more java problems

---
+++ src/main/java/String/LastTwo.java
@@ -0,0 +1,28 @@
+package String;
+
+public class LastTwo {
+    public static void main(String[] args) {
+        System.out.println(lastTwo("coding"));
+    }
+        public static String lastTwo(String str) {
+            String newString = "";
+        String  newString2 = "";
+        String newString3="";
+        if(str.length()<2){
+            return str;
+        }
+        if (str.length() >= 2) {
+            newString2= str.substring(str.length() - 1);
+            System.out.println(newString2);
+            newString3 =str.substring(str.length()-2,str.length()-1);
+            System.out.println(newString3);
+            newString=newString+newString2+newString3;
+            System.out.println(newString);
+
+
+
+
+        }
+        return newString;
+    }
+}
---
+++ src/main/java/String/RepeatSeparator.java
@@ -0,0 +1,27 @@
+package String;
+
+/**
+ * Questions:
+ * Given two strings, word and a separator sep, return a big string made of count occurrences of the word,
+ * separated by the separator string.
+ * Examples:
+ * repeatSeparator("Word", "X", 3) → "WordXWordXWord"
+ * repeatSeparator("This", "And", 2) → "ThisAndThis"
+ * repeatSeparator("This", "And", 1) → "This"
+ */
+public class RepeatSeparator {
+    public String repeatSeparator(String word, String sep, int count) {
+
+            String newString="";
+
+            for (int i =0; i <count; i++){
+                if(i==count-1){
+                    newString=newString+word;
+                }else{
+                    newString=newString+word+sep;
+                }
+            }
+            return newString;
+        }
+
+    }
