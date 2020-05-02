0b7ea588ca0486a432589464a0ef4b917eb5c64b
Antara <antaramimi@gmail.com>
2020-05-02 12:50:40 +0100

    Adding more java problems

---
+++ src/main/java/Collection/Employee.java
@@ -0,0 +1,18 @@
+package Collection;
+
+public class Employee {
+    String name;
+    int age;
+    String deparment;
+
+    Employee(String name, int age, String deparment) {
+        this.name = name;
+        this.age = age;
+        this.deparment = deparment;
+    }
+
+    @Override
+    public String toString() {
+        return name+" "+age+" "+deparment;
+    }
+}
