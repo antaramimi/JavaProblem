573b4c1ea5adb2717423ee5c29cdea639f1b51ea
Antara <antaramimi@gmail.com>
2020-06-13 20:13:08 +0100

    Adding more java problems

--- src/main/java/miscellaneousproblems/ReverseStringInPlace.java
+++ src/main/java/miscellaneousproblems/ReverseStringInPlace.java
@@ -2,25 +2,34 @@ package miscellaneousproblems;
 
 public class ReverseStringInPlace {
     public static void main(String[] args) {
-        reverseString("I Hate you");
+        char[] arr="I hello".toCharArray();
+        char[] arr1="Places".toCharArray();
+        char[] arr2="Ram".toCharArray();
+        System.out.println(arr);
+        reverseString(arr);
+        System.out.println(arr);
+        System.out.println("****");
+        System.out.println(arr1);
+        reverseString(arr1);
+        System.out.println(arr1);
+        System.out.println("*****");
+        System.out.println(arr2);
+        reverseString(arr2);
+        System.out.println(arr2);
     }
 
-    public static void reverseString(String strs) {
-
-        for (int i = 0; i < strs.length(); i++) {
-            reverseCharacter(strs, i, strs.length() - 1);
-
+    public static void reverseString(char[] chars) {
+        for (int i = 0; i < chars.length / 2; i++) {
+            reverseCharacter(chars, i, chars.length - i - 1);
         }
     }
 
-    public static String reverseCharacter(String str, int i, int j) {
-        char[] myArray = str.toCharArray();
+    public static void reverseCharacter(char[] charArray,
+                                        int i, int j) {
         char temp;
-        temp = myArray[i];
-        myArray[i] = myArray[j];
-        myArray[j] = temp;
-        return new String(myArray);
-
+        temp = charArray[i];
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
     }
 
 
