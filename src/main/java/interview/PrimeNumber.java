e11b437acc927ce8911c746a8917353b5b390851
Antara <antaramimi@gmail.com>
2020-06-30 22:54:11 +0100

    Adding more java problems

--- src/main/java/interview/PrimeNumber.java
+++ src/main/java/interview/PrimeNumber.java
@@ -3,22 +3,37 @@ package interview;
 public class PrimeNumber {
     public static void main(String[] args) {
         primeNumber();
+        printPrimeNumber();
     }
 
     public static void primeNumber() {
-        int number=3;
+        int number = 3;
         int temp = 0;
 
-        for(int i=2;i<number-1;i++){
-            if(number%i==0){
-                temp+=1;
+        for (int i = 2; i < number - 1; i++) {
+            if (number % i == 0) {
+                temp += 1;
             }
         }
-        if(temp>0){
+        if (temp > 0) {
             System.out.println("number is not prime");
-        }
-        else {
+        } else {
             System.out.println("number is prime");
+        }
+    }
+
+    public static void printPrimeNumber() {
+        for(int number=2;number<=50;number++){
+            boolean isPrime = true;
+            for(int i=2;i<=number/2;i++){
+                if(number%i==0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime==true){
+                System.out.println(number);
+            }
         }
     }
 }
