cf181549e672184d2c29ab5e05aef2f4ce024fb6
Antara <antaramimi@gmail.com>
2020-07-08 18:50:29 +0100

    Adding more java problems

--- src/main/java/miscellaneousproblems/PasswordValidator.java
+++ src/main/java/miscellaneousproblems/PasswordValidator.java
@@ -11,51 +11,37 @@ import java.util.Scanner;
 
 public class PasswordValidator {
 
-    String name;
-    String password;
     private static boolean valid;
     private static String errorMessage;
-
+    private static Scanner scanner = new Scanner(System.in);
+    String name;
+    String password;
 
     public PasswordValidator(String name, String password) {
         this.name = name;
         this.password = password;
     }
 
-    public boolean isValid(){
-        return valid;
-    }
-
-    public String isErrorMessage(){
-        return errorMessage;
-    }
-
-
-
-    private static Scanner scanner = new Scanner(System.in);
-
-
-    public static  void main(String[] args) {
-        PasswordValidator validator= login();
+    public static void main(String[] args) {
+        PasswordValidator validator = login();
         validator.printPasswordRules();
 
         do {
-           String proposedPassword  =validator.getProposedPassword();
-           validator.changePassword(proposedPassword);
-            if(!validator.isValid()){
+            String proposedPassword = validator.getProposedPassword();
+            validator.changePassword(proposedPassword);
+
+            if (!validator.isValid()) {
                 System.out.println(validator.isErrorMessage());
             }
-        }while (!validator.isValid());
+
+        } while (!validator.isValid());
 
         System.out.println("The proposed password is valid");
 
+        validator.closeScanner();
     }
 
-
-
-
-
-    public static PasswordValidator login(){
+    public static PasswordValidator login() {
         System.out.println("Enter your username");
         String username = scanner.nextLine();
 
@@ -63,11 +49,24 @@ public class PasswordValidator {
         String password = scanner.nextLine();
 
 
-        return new PasswordValidator(username,password);
+        return new PasswordValidator(username, password);
 
 
     }
-    public void printPasswordRules(){
+
+    public boolean isValid() {
+        return valid;
+    }
+
+    public String isErrorMessage() {
+        return errorMessage;
+    }
+
+    public void closeScanner() {
+        scanner.close();
+    }
+
+    public void printPasswordRules() {
         System.out.println("Your new password must meet the following requirements:");
         System.out.println("* at least 8 characters long");
         System.out.println("* contain an uppercase letter");
@@ -76,29 +75,36 @@ public class PasswordValidator {
         System.out.println();
     }
 
-    public String getProposedPassword(){
+    public String getProposedPassword() {
         System.out.println("Enter your new password:");
         return scanner.nextLine();
     }
 
 
-    public  void changePassword(String text) {
+    public void changePassword(String newPassword) {
+
         valid = true;
         errorMessage = "";
 
-
-        if (text.length() < 8) {
+        if (newPassword.length() < 8) {
             valid = false;
-                errorMessage+= "Your password must be at least 8 characters." ;
-            }
-            if (text.equals(text.toLowerCase())){
-                valid = false;
-                errorMessage+="Your password must include an uppercase letter.";
-            }
-            if(text.matches("[a-zA-Z0-9 ]*")){
-                valid = true;
-                System.out.println("Your password must include a special character (e.g. %,$[:). ");
-            }
+            errorMessage += "\n Your password must be at least 8 characters.";
+        }
+
+        if (newPassword.equals(newPassword.toLowerCase())) {
+            valid = false;
+            errorMessage += "\n Your password must include an uppercase letter.";
+        }
+
+        if (newPassword.matches("[a-zA-Z0-9 ]*")) {
+            valid = false;
+            errorMessage += "\n Your password must include a special character (e.g. %,$[:).";
+        }
+        if (newPassword.equals(password)) {
+            valid = false;
+            errorMessage += "\n Your password must be different from your current password.";
         }
     }
+}
+
 
